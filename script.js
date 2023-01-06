6c6318f92864bcab86c65e1359d70dc2791fe5bf
GuiSegatto <guisegatto23@gmail.com>
2023-01-06 20:10:51 -0300

    add completed mark

--- script.js
+++ script.js
@@ -14,8 +14,63 @@ for (i = 0; i < todoList.length; i++) {
 const removeBtn = document.getElementsByClassName("remove-button");
 
 for (i = 0; i < removeBtn.length; i++) {
-    removeBtn[i].onclick = function() {
+    removeBtn[i].onclick = function () {
         let todo = this.parentElement;
-        todo.remove();
-  }
+        let wrapper = todo.parentElement;
+        wrapper.remove();
+    }
 }
+
+// Create the check circle before the todo name
+for (i = 0; i < todoList.length; i++) {
+    circle = document.createElement("div");
+    circle.className = "circle";
+    li = todoList[i]
+    divPai = li.parentElement
+    divPai.insertBefore(circle, li)
+}
+
+// Toggle checked mark
+const list = document.querySelector("ul");
+
+list.addEventListener('click', function (event) {
+    if (event.target.tagName === "LI") {
+        event.target.classList.toggle('checked');
+        let wrapper = event.target.parentNode
+        wrapper.childNodes[1].classList.toggle('checked');
+        let checkIcon = document.createElement('div');
+        checkIcon.classList.add('check-icon')
+
+        toggleIcon()
+        function toggleIcon() {
+            if (event.target.classList.contains('checked')) {
+                wrapper.childNodes[1].appendChild(checkIcon)
+            } else {
+                let icon = wrapper.childNodes[1].childNodes[0]
+                wrapper.childNodes[1].removeChild(icon)
+            }
+        }
+    }
+    else if (event.target.classList.contains("circle")) {
+        event.target.classList.toggle('checked');
+        let wrapper = event.target.parentNode
+        wrapper.childNodes[2].classList.toggle('checked');
+
+        let checkIcon = document.createElement('div');
+        checkIcon.classList.add('check-icon')
+        wrapper.childNodes[1].appendChild(checkIcon)
+    }
+    else if (event.target.classList.contains("check-icon")) {
+        let circle = event.target.parentNode
+        let wrapper = circle.parentNode
+        wrapper.childNodes[2].classList.toggle('checked');
+        circle.classList.toggle('checked')
+        let icon = wrapper.childNodes[1].childNodes[0]
+        wrapper.childNodes[1].removeChild(icon)
+    }
+}, false);
+
+
+// Create a new item and add it to the list
+
+
