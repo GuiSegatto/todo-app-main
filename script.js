b2b54d82fbaae818e91bbb44d4782a9a03ceb84e
GuiSegatto <guisegatto23@gmail.com>
2023-01-06 22:54:41 -0300

    add function to add new todos

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-
+// Global Selectors
+const ul = document.getElementById("todoList");
+const li = document.querySelectorAll('li');
+const removeBtn = document.getElementsByClassName("remove-button");
 
 // Create a close button for each item in the list
-const todoList = document.querySelectorAll('li')
 
-for (i = 0; i < todoList.length; i++) {
+for (i = 0; i < li.length; i++) {
     let crossIcon = document.createElement("img");
     crossIcon.src = "./images/icon-cross.svg";
     crossIcon.className = "remove-button";
-    todoList[i].appendChild(crossIcon);
+    li[i].appendChild(crossIcon);
 }
 
 // Click on close button to remove the element from the list
-const removeBtn = document.getElementsByClassName("remove-button");
-
 for (i = 0; i < removeBtn.length; i++) {
     removeBtn[i].onclick = function () {
         let todo = this.parentElement;
@@ -22,55 +22,116 @@ for (i = 0; i < removeBtn.length; i++) {
 }
 
 // Create the check circle before the todo name
-for (i = 0; i < todoList.length; i++) {
+/* for (i = 0; i < todoList.length; i++) {
     circle = document.createElement("div");
     circle.className = "circle";
     li = todoList[i]
     divPai = li.parentElement
     divPai.insertBefore(circle, li)
+} */
+
+
+// When press "enter" inside input execute newtodo function
+document.getElementById('newItem').onkeydown = function (e) {
+    if (!e) e = window.event;
+    var keyCode = e.code || e.key;
+    if (keyCode == 'Enter' || keyCode == 'NumpadEnter') {
+        newTodo();
+        return false;
+    }
 }
-
-// Toggle checked mark
-const list = document.querySelector("ul");
-
-list.addEventListener('click', function (event) {
-    if (event.target.tagName === "LI") {
-        event.target.classList.toggle('checked');
-        let wrapper = event.target.parentNode
-        wrapper.childNodes[1].classList.toggle('checked');
-        let checkIcon = document.createElement('div');
-        checkIcon.classList.add('check-icon')
-
-        toggleIcon()
-        function toggleIcon() {
-            if (event.target.classList.contains('checked')) {
-                wrapper.childNodes[1].appendChild(checkIcon)
-            } else {
-                let icon = wrapper.childNodes[1].childNodes[0]
-                wrapper.childNodes[1].removeChild(icon)
-            }
-        }
-    }
-    else if (event.target.classList.contains("circle")) {
-        event.target.classList.toggle('checked');
-        let wrapper = event.target.parentNode
-        wrapper.childNodes[2].classList.toggle('checked');
-
-        let checkIcon = document.createElement('div');
-        checkIcon.classList.add('check-icon')
-        wrapper.childNodes[1].appendChild(checkIcon)
-    }
-    else if (event.target.classList.contains("check-icon")) {
-        let circle = event.target.parentNode
-        let wrapper = circle.parentNode
-        wrapper.childNodes[2].classList.toggle('checked');
-        circle.classList.toggle('checked')
-        let icon = wrapper.childNodes[1].childNodes[0]
-        wrapper.childNodes[1].removeChild(icon)
-    }
-}, false);
 
 
 // Create a new item and add it to the list
+function newTodo() {
+    let li = document.createElement("li")
+    let wrapper = document.createElement("div")
+    wrapper.classList.add('todo-wrapper')
 
 
+    let inputValue = document.getElementById('newItem').value;
+
+    let todo = document.createTextNode(inputValue);
+    li.appendChild(todo);
+    if (inputValue === '') {
+        alert("You must write something!");
+    } else {
+        ul.appendChild(wrapper)
+        wrapper.appendChild(li)
+    }
+    document.getElementById("newItem").value = "";
+
+    // Create the check circle before the todo
+    let circle = document.createElement("div")
+    circle.classList.add("circle")
+    wrapper.insertBefore(circle, wrapper.firstChild)
+
+
+
+
+    // Create close button
+    let crossIcon = document.createElement("img");
+    crossIcon.src = "./images/icon-cross.svg";
+    crossIcon.className = "remove-button";
+    li.appendChild(crossIcon);
+
+    for (i = 0; i < removeBtn.length; i++) {
+        removeBtn[i].onclick = function () {
+            let todo = this.parentElement;
+            let wrapper = todo.parentElement;
+            wrapper.remove();
+        }
+    }
+}
+
+
+// Click on close button to remove the element from the list
+
+
+
+// Set the item as complete
+ul.addEventListener('click', function (event) {
+    if (event.target.tagName === "LI" && event.target.tagName !== "img") {
+        event.target.classList.toggle('complete');
+
+        let wrapper = event.target.parentNode;
+        wrapper.childNodes[0].classList.toggle('complete');
+
+        let checkIcon = document.createElement('div');
+        checkIcon.classList.add('check-icon');
+
+        toggleIcon()
+        function toggleIcon() {
+            if (event.target.classList.contains('complete')) {
+                wrapper.childNodes[0].appendChild(checkIcon);
+            } else {
+                let icon = wrapper.childNodes[0].childNodes[0];
+                wrapper.childNodes[0].removeChild(icon);
+            }
+        }
+    }
+
+    else if (event.target.classList.contains("circle")) {
+        event.target.classList.toggle('complete');
+        let wrapper = event.target.parentNode;
+        wrapper.childNodes[1].classList.toggle('complete');
+        let checkIcon = document.createElement('div');
+        checkIcon.classList.add('check-icon')
+        wrapper.childNodes[0].appendChild(checkIcon)
+    }
+
+    else if (event.target.classList.contains("check-icon")) {
+        let circle = event.target.parentNode;
+        let wrapper = circle.parentNode;
+        wrapper.childNodes[1].classList.toggle('complete')
+        circle.classList.toggle('complete')
+        let icon = wrapper.childNodes[0].childNodes[0]
+        wrapper.childNodes[0].removeChild(icon)
+    }
+
+
+
+}, false);
+
+
+// Count how many items on the list are left
